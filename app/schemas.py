3bf7c9bc60f8675d064b39e59a91849c306b669b
abdallahh07 <abdallahhashad029@gmail.com>
2026-08-04 15:54:34 +0300

    add schemmas

---
+++ app/schemas.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from predict import make_prediction
+
+app = FastAPI()
+
+class PredictionInput(BaseModel):
+    oil_brent: float
+    natural_gas: float
+    wheat: float
+    corn: float
+    gold: float
+    aluminum: float
+    iron_ore: float
+    is_covid_period: int
+    is_russia_ukraine_period: int
+    is_red_sea_period: int
+    is_hormuz_period: int
+
+@app.post("/predict")
+def predict(input_data: PredictionInput):
+    result = make_prediction(input_data.dict())
+    return {"prediction": result}
